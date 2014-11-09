2aa280207c6d4df6174b5c5bc478af634d31a34c
Ringo Bautista <bautistarin@gmail.com>
2014-11-09 22:28:31 +0800

    homepage blocks

--- assets/themes/boxy/js/scripts.js
+++ assets/themes/boxy/js/scripts.js
@@ -6,27 +6,12 @@ $(document).ready(function(){
 		var contentOffset = $('.site').offset();
 		if($(window).scrollTop() > contentOffset.top){
 			$('header.main').removeClass('fadeInDown fadeOutUp')
-				.addClass('fadeInDown navbar-fixed-top');
+				.addClass('fadeInDown');
 		}
-
-		if($(window).scrollTop() < contentOffset.top && $(window).scrollTop() > menuOffset.top){
-			$('header.main').removeClass('fadeInDown fadeOutUp')
-				.addClass('fadeOutUp')
-				.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-					$(this).removeClass('navbar-fixed-top');
-				});
-		}
-
-		if($(window).scrollTop() < menuOffset.top){
-			$('header.main').removeClass('fadeInDown fadeOutUp navbar-fixed-top')
-				.addClass('fadeInDown')
-		}
-
-
 	});
 
 	//lazyloading
-	var postsPerPage = 10;
+	var postsPerPage = 8;
 	var postsCount = postsPerPage;
 	var totalPosts = $('.posts li').length;
 
@@ -51,34 +36,40 @@ $(document).ready(function(){
 		var block = i % 10;
 		var color = ['rgba(200,200,200,1)','rgba(195,195,195,1)','rgba(190,190,190,1)','rgba(205,205,205,1)','rgba(210,210,210,1)','rgba(215,215,215,1)','rgba(220,220,220,1)','rgba(225,225,225,1)','rgba(230,230,230,1)','rgba(235,235,235,1)']
 		switch(block){
-			case 2:
-			case 4:
+			// case 2:
+			// case 4:
+			// 	$(this).addClass('col-sm-3')
+			// 		.height(400)
+			// 		.css('background-color',color[Math.floor(Math.random() * 10)]);
+			// 	$(this).find('img').wrap( $('<div class="img-wrapper">').css('height','30%') );
+			// 	break;
+			// case 1:
+			// case 5:
+			// case 6:
+			// case 7:
+			// 	$(this).addClass('col-sm-4')
+			// 		.height(400)
+			// 		.css('background-color', color[Math.floor(Math.random() * 10)]);
+			// 	$(this).find('img').wrap( $('<div class="img-wrapper">').css('height','40%') );
+			// 	break;
+			// case 0:
+			// 	$(this).addClass('col-sm-8')
+			// 		.height(400)
+			// 		.css('background-color', color[Math.floor(Math.random() * 10)]);
+			// 	$(this).find('img').wrap( $('<div class="img-wrapper">').css('height','60%') );
+			// 	break;
+			// case 3:
+			// case 8:
+			// case 9:
+			// 	$(this).addClass('col-sm-6')
+			// 		.height(400)
+			// 		.css('background-color', color[Math.floor(Math.random() * 10)]);
+			// 	$(this).find('img').wrap( $('<div class="img-wrapper">').css('height','50%') );
+			// 	break;
+			default:
 				$(this).addClass('col-sm-3')
 					.height(400)
 					.css('background-color',color[Math.floor(Math.random() * 10)]);
-				$(this).find('img').wrap( $('<div class="img-wrapper">').css('height','30%') );
-				break;
-			case 1:
-			case 5:
-			case 6:
-			case 7:
-				$(this).addClass('col-sm-4')
-					.height(400)
-					.css('background-color', color[Math.floor(Math.random() * 10)]);
-				$(this).find('img').wrap( $('<div class="img-wrapper">').css('height','40%') );
-				break;
-			case 0:
-				$(this).addClass('col-sm-8')
-					.height(400)
-					.css('background-color', color[Math.floor(Math.random() * 10)]);
-				$(this).find('img').wrap( $('<div class="img-wrapper">').css('height','60%') );
-				break;
-			case 3:
-			case 8:
-			case 9:
-				$(this).addClass('col-sm-6')
-					.height(400)
-					.css('background-color', color[Math.floor(Math.random() * 10)]);
 				$(this).find('img').wrap( $('<div class="img-wrapper">').css('height','50%') );
 				break;
 		}
